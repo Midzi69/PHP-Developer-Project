def49297d7828b7ac6275236ce732ec983812517
Miloš Tordai <76227243+Midzi69@users.noreply.github.com>
2023-10-19 14:08:43 +0200

    Update fund.blade.php

--- resources/views/fund.blade.php
+++ resources/views/fund.blade.php
@@ -4,9 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fund Information</title>
-    <style>
-        /* Add your CSS styles for the PDF here */
-    </style>
 </head>
 <body>
     <h1>Fund Information</h1>
